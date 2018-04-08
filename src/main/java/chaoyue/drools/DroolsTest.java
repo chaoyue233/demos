71f385e6c2ebbf62303e8207d25ad0f1cb630886
wuchaoyue <beberoy@sina.com>
2018-04-08 14:58:09 +0800

    并没啥软用的demo

---
+++ src/main/java/chaoyue/drools/DroolsTest.java
@@ -0,0 +1,15 @@
+package chaoyue.drools;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class DroolsTest {
+    public void testHelloWorld() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.newKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession("helloWorldSession");
+        kieSession.fireAllRules();
+        kieSession.dispose();
+    }
+}
